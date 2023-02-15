f2c1d2a3664ba2689d9acd08b25836836b123370
Majumdar <SMajumdar@luxoft.com>
2023-02-15 17:58:21 +0530

    lazy loading added

--- src/AppRoot/AppRoot.js
+++ src/AppRoot/AppRoot.js
@@ -1,16 +1,21 @@
-import React from "react";
+import React, { Suspense } from "react";
 import { Routes, Route } from "react-router-dom";
+import LoaderComponent from "../components/LoaderComponent/LoaderComponent";
 const HomeContainer = React.lazy(() => import("../containers/HomeContainer/HomeContainer"));
 const SearchContainer = React.lazy(() => import("../containers/SearchContainer/SearchContainer"));
 const ViewArticle = React.lazy(() => import("../containers/ViewArticle/ViewArticle"));
 
 const AppRoot = () => {
     return (
-        <Routes>
-            <Route path="/" element={<HomeContainer />} />
-            <Route path="/article/:id" element={<ViewArticle />} />
-            <Route path="/search/:searchVal" element={<SearchContainer />} />
-        </Routes>
+        <>
+            <Suspense fallback={LoaderComponent}>
+                <Routes>
+                    <Route path="/" element={<HomeContainer />} />
+                    <Route path="/article/:id" element={<ViewArticle />} />
+                    <Route path="/search/:searchVal" element={<SearchContainer />} />
+                </Routes>
+            </Suspense>
+        </>
     )
 }
 
